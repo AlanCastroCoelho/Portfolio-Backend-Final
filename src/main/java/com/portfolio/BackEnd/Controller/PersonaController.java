fdad240693bd8ca9e183a7a2d69bd7fdbc570841
Alan Castro Coelho <109250693+AlanCastroCoelho@users.noreply.github.com>
2023-01-15 04:21:20 -0300

    arreglando url del @cross

--- src/main/java/com/portfolio/BackEnd/Controller/PersonaController.java
+++ src/main/java/com/portfolio/BackEnd/Controller/PersonaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://portfolioalancoelho.web.app/portfolio")
+@CrossOrigin(origins = "https://portfolioalancoelho.web.app")
 //@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired
