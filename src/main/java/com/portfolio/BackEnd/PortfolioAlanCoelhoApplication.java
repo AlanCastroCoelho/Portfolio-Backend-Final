930c321ec4e07175c2a510d578f655bd119e8431
Alan Castro Coelho <109250693+AlanCastroCoelho@users.noreply.github.com>
2023-05-16 15:33:30 -0300

    Utilizamos Variables en el App Properties traidas desde el .env

--- src/main/java/com/portfolio/BackEnd/PortfolioAlanCoelhoApplication.java
+++ src/main/java/com/portfolio/BackEnd/PortfolioAlanCoelhoApplication.java
@@ -2,8 +2,10 @@ package com.portfolio.BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:portfolio.env")
 public class PortfolioAlanCoelhoApplication {
 
 	public static void main(String[] args) {
